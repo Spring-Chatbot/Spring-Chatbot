0b2c7473314ff111e94472fc7c5aae164ed17cce
Jennifer Chiang <77jennifer77@users.noreply.github.com>
2020-03-12 15:16:19 -0700

    Basic login prompt

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,19 +2,52 @@ import React from "react";
 import {
     AppBar,
     Toolbar,
-    Typography
+    Typography,
+    Paper,
+    Card,
+    CardActions,
+    CardContent,
+    CardActionArea,
+    Button,
+    Grid,
+    TextField
 } from "@material-ui/core";
 
 class Header extends React.Component{
     render(){
         return (
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6" align='center' className="header">
-                    Welcome to Chatbot
-                  </Typography>
-                </Toolbar>
-            </AppBar>
+            <div>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h6" align='center' className="header">
+                          Welcome to Chatbot
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <Grid container justify="center" style={{ paddingTop: 200 }}>
+                <Grid item xs={3}>
+                <Card className="login">
+                    <CardActionArea>
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2" align="center">
+                              User Login
+                            </Typography>
+                            <TextField required id="standard-required" label="Username" />
+                            <TextField required id="standard-required" label="Password" />
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                        <Button size="small" color="primary" variant="outlined">
+                          Register
+                        </Button>
+                        <Button size="small" color="primary" variant="outlined">
+                          Login
+                        </Button>
+                    </CardActions>
+                </Card>
+                </Grid>
+                </Grid>
+            </div>
         );
     }
 }
