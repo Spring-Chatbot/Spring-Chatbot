a3967258ab9e9f98abd799fe69fa4a8bc4b9c141
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2020-03-12 15:36:39 -0700

    Fixed width issue and EsLint pass

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,7 +3,7 @@ import {
     AppBar,
     Toolbar,
     Typography,
-    Paper,
+    // Paper,
     Card,
     CardActions,
     CardContent,
@@ -20,32 +20,32 @@ class Header extends React.Component{
                 <AppBar position="static">
                     <Toolbar>
                         <Typography variant="h6" align='center' className="header">
-                          Welcome to Chatbot
+                            Welcome to Chatbot
                         </Typography>
                     </Toolbar>
                 </AppBar>
                 <Grid container justify="center" style={{ paddingTop: 200 }}>
-                <Grid item xs={3}>
-                <Card className="login">
-                    <CardActionArea>
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2" align="center">
-                              User Login
-                            </Typography>
-                            <TextField required id="standard-required" label="Username" />
-                            <TextField required id="standard-required" label="Password" />
-                        </CardContent>
-                    </CardActionArea>
-                    <CardActions>
-                        <Button size="small" color="primary" variant="outlined">
-                          Register
-                        </Button>
-                        <Button size="small" color="primary" variant="outlined">
-                          Login
-                        </Button>
-                    </CardActions>
-                </Card>
-                </Grid>
+                    <Grid item xs={2}>
+                        <Card className="login">
+                            <CardActionArea>
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2" align="center">
+                                        User Login
+                                    </Typography>
+                                    <TextField required id="standard-required" label="Username" />
+                                    <TextField required id="standard-required" label="Password" />
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="primary" variant="outlined">
+                                    Register
+                                </Button>
+                                <Button size="small" color="primary" variant="outlined">
+                                    Login
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
                 </Grid>
             </div>
         );
