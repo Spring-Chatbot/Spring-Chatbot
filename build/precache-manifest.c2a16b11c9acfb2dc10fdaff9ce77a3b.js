202286500c8ffd15334a83e6d18c57444930a687
Rachel Lewis <60766726+RLewis627@users.noreply.github.com>
2020-04-13 22:30:05 -0700

    Add files via upload

---
+++ build/precache-manifest.c2a16b11c9acfb2dc10fdaff9ce77a3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e3edbcda023aa4b96841547b89c28f",
+    "url": "/Spring-Chatbot/index.html"
+  },
+  {
+    "revision": "58582f277bf9199ef430",
+    "url": "/Spring-Chatbot/static/css/main.fa28235b.chunk.css"
+  },
+  {
+    "revision": "2586d38617103305b1c6",
+    "url": "/Spring-Chatbot/static/js/2.bfcd2794.chunk.js"
+  },
+  {
+    "revision": "0fb6d03d94b65a8b26ae5d9e06b13372",
+    "url": "/Spring-Chatbot/static/js/2.bfcd2794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58582f277bf9199ef430",
+    "url": "/Spring-Chatbot/static/js/main.9532c921.chunk.js"
+  },
+  {
+    "revision": "9aef97c95ada80748c76",
+    "url": "/Spring-Chatbot/static/js/runtime-main.1c5d4cde.js"
+  }
+]);
