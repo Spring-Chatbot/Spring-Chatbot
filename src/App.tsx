f9be97065d000766c1f7205e336fb24da8bf1144
Rachel Lewis <60766726+RLewis627@users.noreply.github.com>
2020-04-16 22:51:42 -0700

    Update App.tsx
    Thanks component no longer exists

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Route, HashRouter } from 'react-router-dom';
 import Conversation from './components/Conversation';
 import UserLoginBox from './components/UserLoginBox';
 import Register from './components/Register';
-import Thanks from './components/Thanks';
 import "./css/MainStyle.css";
 
 const App = () => {
@@ -12,7 +11,6 @@ const App = () => {
 				<Route exact path='/' component={UserLoginBox} />
 				<Route path='/register' component={Register} />
 				<Route path='/talk' component={Conversation} />
-				<Route path='/thankyou' component={Thanks} />
 		</HashRouter>
     );
 };
