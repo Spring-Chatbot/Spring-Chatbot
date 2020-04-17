8503237d59c445138b8082abf86c0019f27c3ec5
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2020-04-17 01:10:36 -0700

    Formatting update

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -12,26 +12,23 @@
 
 const isLocalhost = Boolean(
     window.location.hostname === "localhost" ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === "[::1]" ||
-    // 127.0.0.0/8 are considered localhost for IPv4.
-    window.location.hostname.match(
-        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
+        // [::1] is the IPv6 localhost address.
+        window.location.hostname === "[::1]" ||
+        // 127.0.0.0/8 are considered localhost for IPv4.
+        window.location.hostname.match(
+            /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+        )
 );
 
 type Config = {
-  onSuccess?: (registration: ServiceWorkerRegistration) => void;
-  onUpdate?: (registration: ServiceWorkerRegistration) => void;
+    onSuccess?: (registration: ServiceWorkerRegistration) => void;
+    onUpdate?: (registration: ServiceWorkerRegistration) => void;
 };
 
 export function register(config?: Config) {
     if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
-    // The URL constructor is available in all browsers that support SW.
-        const publicUrl = new URL(
-            process.env.PUBLIC_URL,
-            window.location.href
-        );
+        // The URL constructor is available in all browsers that support SW.
+        const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
         if (publicUrl.origin !== window.location.origin) {
             // Our service worker won't work if PUBLIC_URL is on a different origin
             // from what our page is served on. This might happen if a CDN is used to
@@ -51,7 +48,7 @@ export function register(config?: Config) {
                 navigator.serviceWorker.ready.then(() => {
                     console.log(
                         "This web app is being served cache-first by a service " +
-              "worker. To learn more, visit https://bit.ly/CRA-PWA"
+                            "worker. To learn more, visit https://bit.ly/CRA-PWA"
                     );
                 });
             } else {
@@ -79,7 +76,7 @@ function registerValidSW(swUrl: string, config?: Config) {
                             // content until all client tabs are closed.
                             console.log(
                                 "New content is available and will be used when all " +
-                  "tabs for this page are closed. See https://bit.ly/CRA-PWA."
+                                    "tabs for this page are closed. See https://bit.ly/CRA-PWA."
                             );
 
                             // Execute callback
@@ -116,7 +113,8 @@ function checkValidServiceWorker(swUrl: string, config?: Config) {
             const contentType = response.headers.get("content-type");
             if (
                 response.status === 404 ||
-        (contentType != null && contentType.indexOf("javascript") === -1)
+                (contentType != null &&
+                    contentType.indexOf("javascript") === -1)
             ) {
                 // No service worker found. Probably a different app. Reload the page.
                 navigator.serviceWorker.ready.then(registration => {
