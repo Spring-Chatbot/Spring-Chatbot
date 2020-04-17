a03839324078d510fec219a250b86fc18a743dc5
Rachel Lewis <60766726+RLewis627@users.noreply.github.com>
2020-04-16 22:47:53 -0700

    Update Conversation.js
    Converted ThankYou component into a function that is integrated into the Conversation component
    Added POST request method to firebase server

--- src/components/Conversation.js
+++ src/components/Conversation.js
@@ -1,25 +1,57 @@
 import React from "react";
 import '../css/ConversationStyle.css';
+import '../css/ThankYouPage.css';
 import $ from 'jquery';
-
-window.$ = $;
+//import firebase from "../firebase.js"
 
 class Conversation extends React.Component {
     constructor(props) {
         super(props);
         this.AppendUserConvo = this.AppendUserConvo.bind(this);
+		this.ThankUser = this.ThankUser.bind(this);
     }
 
     nextPath(path) {
         this.props.history.push(path);
     }
 
+	ThankUser() {
+		document.getElementById("ContainerBox").style.display = "none";
+		document.getElementById("Buttons").style.display = "none";
+		document.getElementById("Welcome").style.display = "none";
+		document.getElementById("thankyou").style.display = "block";
+	}
+
     AppendUserConvo() {
-        var UserComment = document.getElementById("ConvoUserInput").value;
-        document.getElementById("ConvoHistory").innerHTML += '<p id="UserComment">' + UserComment + '</p>';
-        document.getElementById("ConvoHistory").innerHTML += '<p id="TherabotComment">' + "FLARP FLERP FLORP" + '</p>';
-        $('#ConvoHistory').scrollTop($('#ConvoHistory')[0].scrollHeight);
-        $('#ConvoUserInput').val('');
+        const UserComment = document.getElementById("ConvoUserInput").value;
+		//The next 2 comment blocks are the POST request to the firebase server
+		//When performing testing, please keep the POST request commented out
+		//and only use the dummy response, otherwise it will bloat the server 
+		//with a bunch of useless documents
+		
+		/*const myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/json");
+
+		const raw = JSON.stringify({"text":UserComment,"userId":"User1"});
+
+		const requestOptions = {
+		  method: 'POST',
+		  headers: myHeaders,
+		  body: raw,
+		  redirect: 'follow'
+		};
+		*/
+		document.getElementById("ConvoHistory").innerHTML += '<p id="UserComment">'+UserComment+'</p>';
+		/*fetch("https://us-central1-cs-4800-backend-server.cloudfunctions.net/api/message", requestOptions)
+		  .then(response => response.text())
+		  .then(result => document.getElementById("ConvoHistory").innerHTML += '<p id="TherabotComment">'+JSON.parse(result).message+'</p>')
+		  .catch(error => console.log('error', error));
+		*/
+		  
+		//DUMMY RESPONSE | ONLY USE FOR TESTING
+		document.getElementById("ConvoHistory").innerHTML += '<p id="TherabotComment">Flarp Flerp Florp</p>';
+		$('#ConvoHistory').scrollTop($('#ConvoHistory')[0].scrollHeight); 
+		$('#ConvoUserInput').val('');
     }
 
   render() {
@@ -39,7 +71,7 @@ class Conversation extends React.Component {
           <button
             id="LeaveRoom"
             type="button"
-            onClick={() => this.nextPath('/thankyou')}
+            onClick={this.ThankUser}
           >
             Leave Chatroom
           </button>
@@ -50,6 +82,9 @@ class Conversation extends React.Component {
             Submit
           </button>
         </div>
+		<div id = "thankyou" style={{display: "none"}}>
+			<p>Thank you for using Therabot!</p>
+		</div>
       </div>
     );
   }
